7d1383121044e9499318cbbc06dd8988a30ae1aa
RadityaMulya <longinus1411@gmail.com>
2023-12-13 20:27:51 +0800

    Commit

---
+++ latihan_kampus.py
@@ -0,0 +1,41 @@
+# Exercise 1 (Python Literal)
+# Apa tipe-tipe literal dari contoh berikut ini:
+# "Hello ", "007"
+
+literal_1 = "Hello "
+literal_2 = "007"
+
+# Menampilkan tipe-tipe literal
+print("Tipe dari literal 1:", type(literal_1))
+print("Tipe dari literal 2:", type(literal_2))
+
+# Exercise 2
+# Tipe-tipe literal dari empat contoh berikut:
+# "1.5", 2.0, 528, False
+
+literal_1 = "1.5"
+literal_2 = 2.0
+literal_3 = 528
+literal_4 = False
+
+# Menampilkan tipe-tipe literal
+print("Tipe dari literal 1:", type(literal_1))
+print("Tipe dari literal 2:", type(literal_2))
+print("Tipe dari literal 3:", type(literal_3))
+print("Tipe dari literal 4:", type(literal_4))
+
+# Exercise 3
+# Mengonversi bilangan biner ke desimal
+binary_number = "1011"
+decimal_value = int(binary_number, 2)
+print("Nilai desimal dari", binary_number, "adalah:", decimal_value)
+
+# Exercise 1 (Arithmetic Operators and The Hierarchy of Priorities)
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+# Exercise 2
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+# Exercise 3
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+
