7dc4013368bf38ebe3d52d8813ada9bb60301d5e
RadityaMulya <longinus1411@gmail.com>
2023-12-14 08:57:01 +0800

    Commit

--- latihan_kampus.py
+++ latihan_kampus.py
@@ -79,4 +79,67 @@ print(x * "5")
 x = input("Enter a number: ")  # The user enters 2
 print(type(x))
 
+# Exercise 1 (Comparison Oprators and Conditional Executions)
+x = 5
+y = 10
+z = 8
+
+print(x > y)
+print(y > z)
+
+# Exercise 2
+x, y, z = 5, 10, 8
+
+print(x > z)
+print((y - 5) == x)
+
+# Exercise 3
+x, y, z = 5, 10, 8
+x, y, z = z, y, x
+
+print(x > z)
+print((y - 5) == x)
+
+# Exercise 4
+x = 10
+
+if x == 10:
+    print(x == 10)
+if x > 5:
+    print(x > 5)
+if x < 10:
+    print(x < 10)
+else:
+    print("else")
+
+# Exercise 5
+x = "1"
+
+if x == 1:
+    print("one")
+elif x == "1":
+    if int(x) > 1:
+        print("two")
+    elif int(x) < 1:
+        print("three")
+    else:
+        print("four")
+if int(x) == 1:
+    print("five")
+else:
+    print("six")
+
+# Exercise 6
+x = 1
+y = 1.0
+z = "1"
+
+if x == y:
+    print("one")
+if y == int(z):
+    print("two")
+elif x == y:
+    print("three")
+else:
+    print("four")
 
